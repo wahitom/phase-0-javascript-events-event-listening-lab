4f28e25abd6f837230d0288382f6122384ed032c
Miriam Wahito Maina <miriammaina51@gmail.com>
2023-09-30 00:38:44 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-function addingEventListener() {
+function addingEventListener(){
+    const input = document.getElementById('input')
+
+    input.addEventListener('mouseover', function(){
+        console.log("something came over me!")
+    })
 }
+
+addingEventListener();
